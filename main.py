3789b406eeb4241e287926c6a768ad2d910fe9c2
iotarepeat <dhruvam15@gmail.com>
2019-01-18 16:35:28 +0530

    1) Changed probablity for spawning random 2)  0 is not displayed 3) Fixed some bugs in moves 4) Check if move is valid before spawning

--- main.py
+++ main.py
@@ -11,48 +11,54 @@ class Main:
 
 	@staticmethod
 	def perform_move(l):
-		for i in range(len(l)):
-			if l[i] == 0:
-				del l[i]
-				l.insert(0, 0)
-		if l.count(0) >= 3:
-			return l
-		n = l[1:]
-		for i in range(len(l) - 1):
-			if l[i] == n[i]:
-				l[i] *= 2
-				l[i + 1] = 0
-		for i in range(len(l)):
-			if l[i] == 0:
-				del l[i]
-				l.insert(0, 0)
+		for i in range(3):
+			try:
+				l.remove(0)
+			except ValueError:
+				break
+			l.append(0)
+		for ind, i in enumerate(l[:-1]):
+			n = l[1:] + [0]
+			if i == n[ind]:
+				l[ind] *= 2
+				l[ind + 1] = 0
+				n[ind + 1] = 0
+				l.remove(0)
+				l.append(0)
 		return l
 
 	def key(self, event):
-		if event.keysym == "Right":
+		prev_board = []
+		prev_board = self.board
+		if event.keysym == "Left":
 			self.board = list(map(self.perform_move, self.board))
 
-		if event.keysym == "Left":
+		if event.keysym == "Right":
 			self.board = list(
 			    map(lambda x: self.perform_move(x[::-1])[::-1], self.board))
-		if event.keysym == "Up":
-			transpose = lambda l_of_l:[list(l) for l in zip(*l_of_l)]
-			self.board = list(map(lambda x: self.perform_move(x[::-1])[::-1],transpose(self.board)))
+		if event.keysym == "Down":
+			transpose = lambda l_of_l: [list(l) for l in zip(*l_of_l)]
+			self.board = list(
+			    map(lambda x: self.perform_move(x[::-1])[::-1],
+			        transpose(self.board)))
 			self.board = transpose(self.board)
 
-		if event.keysym == "Down":
-			transpose = lambda l_of_l:[list(l) for l in zip(*l_of_l)]
-			self.board = list(map(self.perform_move,transpose(self.board)))
+		if event.keysym == "Up":
+			transpose = lambda l_of_l: [list(l) for l in zip(*l_of_l)]
+			self.board = list(map(self.perform_move, transpose(self.board)))
 			self.board = transpose(self.board)
+		for i, j in zip(prev_board, self.board):
+			if i != j:
+				self.spawn()
+				break
 		self.display()
 
 	def display(self):
-		self.spawn()
 		for i in range(len(self.board)):
 			for j in range(len(self.board)):
 				tk.Label(
 				    self.root,
-				    text=self.board[i][j],
+				    text=self.board[i][j] if self.board[i][j] else None,
 				    height=5,
 				    width=10,
 				    borderwidth=2,
@@ -64,12 +70,10 @@ class Main:
 		'''
 			Randomly sets a single zero element in board to 2,4 or 8
 		'''
-		num = random.choice([2, 4, 8])
+		num = random.choice([2] * 8 + [4] * 2)
 		index = [(i, j) for i in range(len(self.board))
-		         for j in range(len(self.board))
+		         for j in range(len(self.board)) if self.board[i][j] == 0
 		         ]  # Generate all possible rows and columns
-		index = list(filter(lambda x: not self.board[x[0]][x[1]],
-		                    index))  # Filter out non zero rows
 		if len(index) > 0:
 			index = random.choice(index)
 			self.board[index[0]][index[1]] = num
@@ -79,5 +83,6 @@ class Main:
 
 
 m = Main()
+m.spawn()
 m.display()
 m.root.mainloop()
