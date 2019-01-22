9cf984ce2c879802ec31ba3e14e7666911f184f1
iotarepeat <dhruvam15@gmail.com>
2019-01-22 17:09:48 +0530

    Fixed bugs in bestMove

--- bot.py
+++ bot.py
@@ -8,16 +8,12 @@ class Bot(Main):
 		self.puzzle.bind("<Key>", self.key)
 
 	def key_mod(self, event):
-		move = "<%s>" % (self.bestMove({
-		    "Up": 0,
-		    "Down": 0,
-		    "Left": 0,
-		    "Right": 0
-		}))
+		move = "<%s>" % (self.bestMove(self.board,2)[0])
 		print(move)
 		self.puzzle.event_generate(move)
+		self.display()
 
-	def predict(self,board, direction):
+	def predict(self, board, direction):
 		old = [i.copy() for i in board]
 		if direction == "Left":
 			new_board = list(map(self.perform_move, board))
@@ -30,39 +26,57 @@ class Bot(Main):
 			new_board = list(
 			    map(lambda x: self.perform_move(x[::-1])[::-1],
 			        transpose(board)))
-			new_board = transpose(board)
+			new_board = transpose(new_board)
 
 		elif direction == "Up":
 			transpose = lambda l_of_l: [list(l) for l in zip(*l_of_l)]
 			new_board = list(map(self.perform_move, transpose(board)))
-			new_board = transpose(board)
+			new_board = transpose(new_board)
 		board = [i.copy() for i in old]
 		return new_board
+	@staticmethod
+	def issorted(board):
+		score = 0
+		for i in board:
+			x = sum(map(lambda x, y: abs(x - y), i, sorted(i))) 
+			if x==0:
+				score+=1
+		for i in zip(*board):
+			x = sum(map(lambda x, y: abs(x - y), i, sorted(i))) 
+			if x==0:
+				score+=1
+		return score
 
-	def bestMove(self, score):
-		board = [i.copy() for i in self.board]
-		prev_board = [i.copy() for i in board]
-		for i in range(3):
-			for move in ["Up", "Down", "Left", "Right"]:
-				board = self.predict(board,move)
-				for i, j in zip(self.board, board):
-					if i != j:
-						break
-				else:
-					score[move]-=1
-					continue
-				score[move] += self.tiles_merged(prev_board, board)
-				prev_board = board
-		return max(score, key=lambda x: score[x])
+	def bestMove(self, board, depth=2):
+		moves = ["Up", "Down", "Left", "Right"]
+		predictions = {}
+		for move in moves:
+			predictions[move] = self.predict(board, move)
+			for i, j in zip(board, predictions[move]):
+				if i != j: break
+			else:
+				del predictions[move] 
+		if depth == 0:
+			l = []
+			for move in predictions:
+				merge = self.tiles_merged(self.board,predictions[move])+self.issorted(predictions[move])
+				m = (move,merge)
+				l.append(m)
+			return max(l,key=lambda x:x[1])
+		l=[]
+		for move in predictions:
+			y = self.bestMove(predictions[move],depth-1)[1]
+			l.append((move,y))
+		return max(l,key=lambda x:x[1])
 
 	@staticmethod
 	def tiles_merged(prev, curr):
 		count = lambda board: sum([1 for i in board for j in i if j != 0])
-		return -(count(curr) - count(prev) - 1)
+		return abs(count(curr) - count(prev))
 
 
 if __name__ == "__main__":
-	m = Bot()
-	m.spawn()
-	m.display()
-	m.puzzle.mainloop()
+	b = Bot()
+	b.spawn()
+	b.display()
+	b.root.mainloop()
