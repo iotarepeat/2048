8bb82e221fa9adc91d981f6eb2847f179d03a191
iotarepeat <dhruvam15@gmail.com>
2019-01-22 14:10:10 +0530

    Documented the code

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ class Main:
 		self.board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 		self.root = tk.Tk()
 		self.puzzle = tk.Frame(self.root)
-		for i in ["Left","Right","Up","Down"]:
-			self.puzzle.bind("<"+i+">", self.key)
+		for i in ["Left", "Right", "Up", "Down"]:
+			self.puzzle.bind("<" + i + ">", self.key)
 		self.puzzle.focus_set()
 		self.puzzle.grid()
 		self.color = {
@@ -29,12 +29,28 @@ class Main:
 
 	@staticmethod
 	def perform_move(l):
+		'''
+			Perform move assuming left on given row
+			EG: 2,2,2,0 becomes 4,2,0,0
+		'''
+		# Append all zero to extreme right
 		for i in range(3):
 			try:
 				l.remove(0)
 				l.append(0)
 			except ValueError:
 				break
+		'''
+			l is original list,
+			n stores next elements corresponding to original
+			EG
+			if l = [1,2,3,4] then n = [2,3,4,0]
+
+			if there exists an index i such that,
+			l[i] ==  n[i]
+			set l[i+1] = 0
+			Again move the zero to extreme right
+		'''
 		for ind, i in enumerate(l[:-1]):
 			n = l[1:] + [0]
 			if i == n[ind]:
@@ -50,23 +66,31 @@ class Main:
 			Perform move based on the key pressed
 		'''
 		prev_board = [i.copy() for i in self.board]
+		transpose = lambda l_of_l: [list(l) for l in zip(*l_of_l)]
 		if event.keysym == "Left":
+			# Apply move to every row
 			self.board = list(map(self.perform_move, self.board))
 
 		elif event.keysym == "Right":
+			# Reverse and apply move to every row and reverse the result
 			self.board = list(
 			    map(lambda x: self.perform_move(x[::-1])[::-1], self.board))
+		elif event.keysym == "Up":
+			# Transpose and apply move to every row and Transpose the result
+			self.board = list(map(self.perform_move, transpose(self.board)))
+			self.board = transpose(self.board)
 		elif event.keysym == "Down":
-			transpose = lambda l_of_l: [list(l) for l in zip(*l_of_l)]
+			# Transpose + Reverse
 			self.board = list(
 			    map(lambda x: self.perform_move(x[::-1])[::-1],
 			        transpose(self.board)))
 			self.board = transpose(self.board)
 
-		elif event.keysym == "Up":
-			transpose = lambda l_of_l: [list(l) for l in zip(*l_of_l)]
-			self.board = list(map(self.perform_move, transpose(self.board)))
-			self.board = transpose(self.board)
+		'''
+			Check if any further moves are possible before
+			spawing
+			
+		'''
 		m = 2
 		zero = False
 		for i, j in zip(prev_board, self.board):
@@ -74,11 +98,13 @@ class Main:
 			if 0 in j:
 				zero = True
 			if i != j:
-				self.spawn()  #If there's no change in the board,do not spawn
+				self.spawn()  #If there's change in the board spawn
 				self.display()
 				break
+		# Check if there are any empty squares
 		if not zero:
-			if not self.game_over():  # Check if any possible moves like left ,up or down can be performed
+		# Check if game is over  Check if any possible moves like left ,up or down can be performed
+			if not self.game_over():
 				self.display()
 			else:
 				self.root.focus_set()
@@ -89,6 +115,8 @@ class Main:
 	def display(self):
 		for i in range(len(self.board)):
 			for j in range(len(self.board)):
+				# Do not display a number for zero,
+				# If non zero choose background color from  dictionary
 				tk.Label(
 				    self.puzzle,
 				    text=self.board[i][j] if self.board[i][j] else None,
@@ -101,6 +129,10 @@ class Main:
 		self.puzzle.update()
 
 	def game_over(self):
+		'''
+			Checks if there are any two adjacent tiles
+			horizontally and vertically
+		'''
 		for i in self.board:
 			prev = i[0]
 			for j in i[1:]:
