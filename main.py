fd9cd6698f24e51c6e6fb1014fb0693d41efe5b6
iotarepeat <dhruvam15@gmail.com>
2019-01-22 17:27:36 +0530

    Added score

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ class Main:
 
 	def __init__(self):
 		self.prev_board = []
+		self.score = 0
 		self.root = tk.Tk()
+		self.root.title("2048")
 		self.puzzle = tk.Frame(self.root)
 		for i in ["Left", "Right", "Up", "Down"]:
 			self.puzzle.bind("<" + i + ">", self.key)
@@ -17,26 +19,25 @@ class Main:
 		self.puzzle.grid()
 		self.puzzle.focus_set()
 		self.color = {
-		    0: "#ccc",
-		    2: "#eee4da",
-		    4: "#ede0c8",
-		    8: "#f2b179",
-		    16: "#f59563",
-		    32: "#f67c5f",
-		    64: "#f65e3b",
-		    128: "#edcf72",
-		    256: "#edcc61",
-		    512: "#edc850",
-		    1024: "#edc53f",
-		    2048: "#edc22e"
+			0: "#ccc",
+			2: "#eee4da",
+			4: "#ede0c8",
+			8: "#f2b179",
+			16: "#f59563",
+			32: "#f67c5f",
+			64: "#f65e3b",
+			128: "#edcf72",
+			256: "#edcc61",
+			512: "#edc850",
+			1024: "#edc53f",
+			2048: "#edc22e"
 		}
 
 	def undo(self, event):
 		self.board = [i.copy() for i in self.prev_board]
 		self.display()
 
-	@staticmethod
-	def perform_move(l):
+	def perform_move(self, l):
 		'''
 			Perform move assuming left on given row
 			EG: 2,2,2,0 becomes 4,2,0,0
@@ -62,6 +63,7 @@ class Main:
 		for ind, i in enumerate(l[:-1]):
 			n = l[1:] + [0]
 			if i == n[ind]:
+				self.score += l[ind]
 				l[ind] *= 2
 				l[ind + 1] = 0
 				n[ind + 1] = 0
@@ -82,7 +84,7 @@ class Main:
 		elif event.keysym == "Right":
 			# Reverse and apply move to every row and reverse the result
 			self.board = list(
-			    map(lambda x: self.perform_move(x[::-1])[::-1], self.board))
+				map(lambda x: self.perform_move(x[::-1])[::-1], self.board))
 		elif event.keysym == "Up":
 			# Transpose and apply move to every row and Transpose the result
 			self.board = list(map(self.perform_move, transpose(self.board)))
@@ -90,18 +92,16 @@ class Main:
 		elif event.keysym == "Down":
 			# Transpose + Reverse
 			self.board = list(
-			    map(lambda x: self.perform_move(x[::-1])[::-1],
-			        transpose(self.board)))
+				map(lambda x: self.perform_move(x[::-1])[::-1],
+					transpose(self.board)))
 			self.board = transpose(self.board)
 		'''
 			Check if any further moves are possible before
 			spawing
 			
 		'''
-		m = 2
 		zero = False
 		for i, j in zip(prev_board, self.board):
-			m = max(m, *j)
 			if 0 in j:
 				zero = True
 			if i != j:
@@ -117,8 +117,9 @@ class Main:
 			else:
 				self.root.focus_set()
 				tk.Label(
-				    self.root,
-				    text="Game Over  Your score is: " + str(m)).grid()
+					self.root,
+					text="Game Over  Your score is: " + str(
+						self.score)).grid()
 
 	def display(self):
 		for i in range(len(self.board)):
@@ -126,14 +127,19 @@ class Main:
 				# Do not display a number for zero,
 				# If non zero choose background color from  dictionary
 				tk.Label(
-				    self.puzzle,
-				    text=self.board[i][j] if self.board[i][j] else None,
-				    bg=self.color[self.board[i][j]],
-				    height=5,
-				    width=10,
-				    borderwidth=2,
-				    relief="ridge").grid(
-				        row=i, column=j)
+					self.puzzle,
+					text=self.board[i][j] if self.board[i][j] else None,
+					bg=self.color[self.board[i][j]],
+					height=5,
+					width=10,
+					borderwidth=2,
+					relief="ridge").grid(
+						row=i, column=j)
+		tk.Label(
+			self.root,
+			text="Score: " + str(self.score),
+		).grid(
+			row=5, column=0, sticky="nsew", rowspan=4)
 		self.puzzle.update()
 
 	def game_over(self):
@@ -161,8 +167,8 @@ class Main:
 		'''
 		num = random.choice([2] * 8 + [4] * 2)
 		index = [(i, j) for i in range(len(self.board))
-		         for j in range(len(self.board)) if self.board[i][j] == 0
-		         ]  # Generate all possible rows and columns
+				 for j in range(len(self.board)) if self.board[i][j] == 0
+				 ]  # Generate all possible rows and columns
 		if len(index) > 0:
 			x, y = random.choice(index)
 			self.board[x][y] = num
